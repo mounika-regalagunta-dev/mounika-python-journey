77a45db38743d7cf4fc93d2e1aace0e3c7555edc
Mounika Regalagunta <mounikaregalagunta@gmail.com>
2025-06-20 16:46:44 +0530

    Add files via upload

---
+++ pstring.py
@@ -0,0 +1,19 @@
+#right
+a="Mounika"
+b=5
+print(a[-b:]+a[:-b])
+# #output:-
+# nikaMou
+
+# a="mounika"
+# # how many rotations/no.of rotations
+# b=15%len(a)
+# print(b)
+
+# #left
+# a="Mounika"
+# b=4
+# print(a[b:]+a[:b])
+# #output:-
+# ikaMoun
+
